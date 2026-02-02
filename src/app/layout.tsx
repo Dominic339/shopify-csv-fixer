d9321627e052637777b20d0088dda300ab493b8c
Dominic339 <Dominator0211@yahoo.com>
2026-02-02 16:30:39 -0500

    small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 // src/app/layout.tsx
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
-import { TopBar } from "@/components/TopBar";
+import TopBar from "@/components/TopBar";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
