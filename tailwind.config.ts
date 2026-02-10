21f3e914dbd6d59cfe7ff13fd42a2263eb5f000e
Dominic339 <Dominator0211@yahoo.com>
2026-02-09 23:14:39 -0500

    validity improvements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
